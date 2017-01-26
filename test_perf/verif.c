49d291657836b620add3da9bb2616d74c9973c9b
lapin <serprev.z@gmail.com>
2017-01-26 19:19:15 +0100

    supression des fichier inutile, simplification des bail

--- test_perf/verif.c
+++ test_perf/verif.c
@@ -4,8 +4,14 @@
 
 #define MAX 300
 
+/*
+**	on peu un peu modifier le main pour verifier que le resultat 
+**	est exacte ou bien cree deux executable pour tester les performace
+**	sur les mac de l'ecole je suis 4 fois plus rapide.
+**	Sur mon linux ubuntu je suis 15 fois lus rapide
+*/
 
-inline	float	pow_f_up(float nb, int pow)
+static inline	double	pow_l_up(double nb, int pow)
 {
 	double	result;
 	double	power;
@@ -16,20 +22,17 @@ inline	float	pow_f_up(float nb, int pow)
 	power = nb;
 	while (i <= pow)
 	{
-		if (i & pow)	
-		{
+		if (i & pow)
 			result *= power;
-		}
-
 		power *= power;
 		i <<= 1;	
 	}
-	return ((float)result);
+	return (result);
 }
 
 int	main(int ac, char **av)
 {
-	float	i;
+	double	i;
 	int	power;
 	double	result;
 	double	r1;
@@ -41,11 +44,11 @@ int	main(int ac, char **av)
 		i = -MAX;
 		while(i < MAX)
 		{
-			r1 = powf(i, power);
-			r2 = pow_f_up(i, power);
+			r1 = pow(i, power);
+			r2 = pow_l_up(i, power);
 			result = r1 - r2;
 			printf("pow_org:%f	pow_opt%f	diff:%f\n", r1, r2, result);
-			i += 0.1;
+			i += 0.00001;
 		}
 	}
 	return (0);
